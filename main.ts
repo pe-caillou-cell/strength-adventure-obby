996a11dd9103d1d690aa8ba41505a672ebf51e09
pe-caillou-cell <pedroestrada@campus.edu.uy>
2025-09-17 17:07:14 -0300

    protagonist sprite

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . e e e e . . . . . . 
+    . . . . . e e e e e e . . . . . 
+    . . . . . . d d d d . . . . . . 
+    . . . . . . f d d f . . . . . . 
+    . . . . . . d f f d . . . . . . 
+    . . . 2 2 d d f f d d 2 2 . . . 
+    . . . 2 2 3 d d d d 3 2 2 . . . 
+    . . . 2 2 d d d d d d 2 2 . . . 
+    . . . 2 2 d d d d d d 2 2 . . . 
+    . . . d 2 d d d d d d 2 d . . . 
+    . . . . 2 e e 5 e e e 2 . . . . 
+    . . . . e e . . . . e e . . . . 
+    . . . . e e . . . . e e . . . . 
+    . . . . e e . . . . e e . . . . 
+    . . . . e e . . . . e e . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+info.setScore(0)
